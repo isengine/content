16d4486b8b3db918de537b74e1da3924ee6b26df
fwmakc <fwmakc@mail.ru>
2022-03-21 22:31:21 +0300

    1.0

--- class/Content.php
+++ class/Content.php
@@ -9,6 +9,7 @@ use is\Helpers\Parser;
 use is\Helpers\Paths;
 use is\Helpers\Prepare;
 use is\Helpers\Local;
+use is\Helpers\Sessions;
 
 use is\Components\Cache;
 use is\Components\Collection;
@@ -39,6 +40,32 @@ class Content extends Master {
 	public $datetime; // объект Datatime
 	public $tvar; // объект обработчика текстовых переменных
 	public $translit; // объект транслитирования строк
+	
+	/*
+	* Мы добавили в настройки:
+	* имена полные, с родителями, в виде массива, для фильтрации
+	* include - включаем только их
+	* exclude - исключаем их
+	* при указании обоих, сначала работает include, потом exclude
+	* так, например, мы можем указать несколько обязательных материалов
+	* и исключить из них текущий
+	*/
+	
+	/*
+	* в сам класс нужно внести доработки, связанные с выводом модуля на странице
+	* дело в том, что если модуль использует родителей, может возникнуть путаница
+	* например, на странице /news/ вызывается материал news1 с родителем news
+	* вроде бы все просто, нужно не учитывать страницу:
+	* /news/news1/
+	* а если материал вызывается на странице /articles/?
+	* ок, можно не учитывать первого родителя:
+	* /articles/news1/
+	* но тогда не будет работать роутинг!
+	* ок, а если материал вызывается на странице /content/news/?
+	* а если материал использует двух и более родителей?
+	* все это нуждается в большой доработке, но уже не в рамках данной версии
+	* в рамках данной версии, мы использует вариант 1, т.е. не учитываем страницу
+	*/
 	
 	public function launch() {
 		
@@ -87,9 +114,21 @@ class Content extends Master {
 			$this -> filter -> init($sets);
 			$this -> filter -> excepts();
 			
+			// очевидно, excepts - это ислючения для фильтрации
+			// чтобы rest запросы типа items, page не попадали в общий фильтр
+			
 			if ($sets['routing']) {
 				$this -> filter -> rest();
 			}
+			
+			// есть ли здесь фильтрация между датами создания и удаления
+			// если нет, то ее нужно добавлять по необходимости, например, в настройки
+			// или выключать, чтобы можно было выводить весь контент, например, в админке
+			// но это нужно делать до сортировки и навигации, очевидно чтобы исключенные материалы
+			// не влияли на уменьшение числа материалов на странице и т.д.
+			
+			// а где же вывод материалов согласно запрошенной странице?
+			// очевидно, должен быть через iterate
 			
 			$this -> check();
 			
@@ -97,37 +136,45 @@ class Content extends Master {
 			$this -> sort($sets['sort']);
 			$this -> list = $this -> data -> getNames();
 			
+			$name = $this -> current ? ($this -> parents ? $this -> parents . ':' : null) . $this -> current : null;
+			
 			$this -> navigate = new Navigate;
 			$this -> navigate -> init($sets);
 			$this -> navigate -> list($this -> list);
-			$this -> navigate -> current($this -> current);
+			$this -> navigate -> current($name);
 			$this -> navigate -> launch();
 			
-			if ($sets['routing'] && $this -> current) {
+			if ($sets['routing'] && $name) {
 				
-				$name = ($this -> parents ? $this -> parents . ':' : null) . $this -> current;
-				//System::debug($name, '!q');
-				
-				$names = $this -> data -> getNames();
 				$map = $this -> data -> map -> count;
 				
-				if (is_array($names) && Objects::match($names, $name)) {
+				if (is_array($this -> list) && Objects::match($this -> list, $name)) {
 					$this -> data -> leaveByName($name);
 				} elseif (is_array($map) && Objects::matchByIndex($map, $name)) {
 					$this -> parents .= ':' . $this -> current;
 					$this -> current = null;
 					$this -> data -> addFilter('parents', '+' . Strings::replace($name, ':', ':+'));
 					$this -> data -> filtration();
-					$this -> data -> leaveByList($this -> data -> getNames(), 'name');
+					$this -> data -> leaveByList($this -> list, 'name');
 				} else {
 					$this -> data -> reset();
 				}
 				
 			} else {
-				$this -> limit($sets['skip'], $sets['limit']);
+				
+				$this -> data -> removeByCut(
+					($this -> navigate -> page - 1) * $sets['limit'] + $sets['skip']
+				);
+				
+				$this -> sort($this -> navigate -> sort ? $this -> navigate -> sort : $sets['sort-after']);
+				
+				$this -> data -> removeByCut(
+					0,
+					$sets['limit']
+				);
+				
 			}
 			
-			$this -> sort($sets['sort-after']);
 			$this -> data -> countMap();
 			
 			$this -> type();
@@ -183,6 +230,21 @@ class Content extends Master {
 		$this -> filter -> filtration($db -> driver -> filter);
 		
 		$db -> launch();
+		
+		if ($this -> settings['include']) {
+			//System::debug($this -> settings['include']);
+			$db -> data -> leaveByList($this -> settings['include'], 'name');
+		}
+		
+		if ($this -> settings['exclude']) {
+			$names = Objects::remove(
+				$db -> data -> getNames(),
+				$this -> settings['exclude']
+			);
+			//System::debug($this -> settings['exclude']);
+			//System::debug($names);
+			$db -> data -> leaveByList($names, 'name');
+		}
 		
 		$this -> data -> addByList( $db -> data -> getData() );
 		$this -> data -> countMap();
@@ -273,7 +335,7 @@ class Content extends Master {
 		if (System::set($this -> current)) {
 			$this -> type = $this -> data -> getFirst() ? 'alone' : 'none';
 		} else {
-			$this -> type = 'list';
+			$this -> type = $this -> navigate -> pages && $this -> navigate -> page > $this -> navigate -> pages ? 'none' : 'list';
 		}
 	}
 	
--- class/Content/Navigate.php
+++ class/Content/Navigate.php
@@ -28,31 +28,41 @@ class Navigate extends Data {
 	public $skip; // число пропущенных материалов от начала
 	
 	public $all; // общее число материалов
-	public $count; // число выводимых материалов
+	public $count; // число выводимых материалов или не задано, если выводятся все
 	
 	public $from; // номер первого выводимого материала
 	
 	public $list; // список всех материалов
 	public $display; // список выводимых материалов с сохранением порядкового номера
 	
-	public $current; // имя текущего материала
-	public $first; // имя первого материала
-	public $last; // имя последнего материала
-	public $prev; // имя предыдущего материала
-	public $next; // имя следующего материала
+	public $id; // номер текущего материала в списке всех, не задано для списка
+	public $current; // имя текущего материала, не задано для списка
+	public $first; // имя первого материала в списке всех
+	public $last; // имя последнего материала в списке всех
+	public $prev; // имя предыдущего материала, не задано для списка
+	public $next; // имя следующего материала, не задано для списка
 	
 	public $page; // номер текущей страницы
-	public $pages; // общее число страниц
+	public $pages; // общее число страниц или не задано, если выводятся все
+	
+	public $sort; // сортировка
 	
 	public $name_page; // служебное поле rest для номера страницы
 	public $name_items; // служебное поле rest для числа материалов на одной странице
+	public $name_sort; // служебное поле rest для окончательной сортировки материалов, вместо sort-after
+	
+	public $rest; // служебное поле, обозначающее задан ли rest или нужно использовать query
+	public $keys; // служебное поле, обозначающее используются ли ключи для rest
 	
 	public function init(&$sets) {
 		
 		$this -> sets = &$sets;
 		
-		$this -> name_page = $sets['rest']['page'];
-		$this -> name_items = $sets['rest']['items'];
+		if ($sets['routing']) {
+			$this -> name_page = $sets['rest']['page'];
+			$this -> name_items = $sets['rest']['items'];
+			$this -> name_sort = $sets['rest']['sort'];
+		}
 		
 		$this -> skip = $this -> sets['skip'];
 		$this -> count = $this -> sets['limit'];
@@ -71,7 +81,8 @@ class Navigate extends Data {
 		
 		if (
 			!$this -> name_page ||
-			!$this -> name_items
+			!$this -> name_items ||
+			!$this -> name_sort
 		) {
 			return;
 		}
@@ -94,38 +105,78 @@ class Navigate extends Data {
 			if ($id !== $last) {
 				$this -> next = $this -> list[$id + 1];
 			}
+			$this -> id = $id;
 		}
-		
-		
-		//$this -> count = $sets['limit'];
 		
 		$uri = Uri::getInstance();
 		
-		$page = $uri -> getData($this -> name_page);
+		// разберемся с сортировкой
+		
+		$this -> sort = $uri -> getData($this -> name_sort);
+		
+		// разберемся с количеством записей на странице
+		
+		// получаем число записей из урла
+		
 		$items = $uri -> getData($this -> name_items);
 		
-		if ($page) {
-			$this -> page = $page;
-		}
-		if ($this -> page < 1) {
-			$this -> page = 1;
+		// сверяем число записей
+		// здесь очень важно проверить, чтобы число записей было именно числом
+		// и если задано число 0, то выводятся все записи без пропусков и лимитов
+		
+		if (System::type($items, 'numeric')) {
+			if ($items) {
+				$this -> count = $items;
+			} else {
+				$this -> count = null;
+				$this -> skip = 0;
+			}
 		}
 		
-		if ($items) {
-			$this -> count = $items;
-		} elseif ($items === 0 || $items === '0') {
-			$this -> count = null;
-			$this -> skip = 0;
+		// разберемся со страницами
+		
+		// получаем общее число номеров страниц
+		
+		$this -> pages = $this -> count ? ceil(($this -> all - $this -> skip) / $this -> count) : null;
+		
+		// получаем номер страницы из урла
+		
+		$page = $uri -> getData($this -> name_page);
+		
+		// сверяем номер страницы
+		// здесь важно делать проверку на вывод списка или одиночного материала
+		// путем проверки this -> current
+		// в одиночном материале мы математически высчитываем номер страницы в списке
+		
+		if ($this -> current) {
+			$page = floor(($this -> id - $this -> skip) / $this -> count) + 1;
 		}
+		
+		if (
+			!System::type($page, 'numeric') ||
+			!$page ||
+			$page < 1
+		) {
+			$page = 1;
+		}
+		
+		// записываем номер страницы
+		
+		$this -> page = $page;
+		
+		// устанавливаем все прочие значения для навигации
 		
 		$this -> from = $this -> skip + ($this -> page - 1) * $this -> count;
 		
 		$this -> display = Objects::get($this -> list, $this -> from, $this -> count);
 		
-		$this -> pages = $this -> count ? ceil(($this -> all - $this -> skip) / $this -> count) : null;
-		
 		$this -> sets['skip'] = $this -> skip;
 		$this -> sets['limit'] = $this -> count;
+		
+		// задаем линки для формирования навигации
+		
+		$this -> rest = $uri -> get('rest');
+		$this -> keys = $uri -> get('keys');
 		
 		//echo '<pre>';
 		//echo '[' . print_r($this, 1) . ']';
